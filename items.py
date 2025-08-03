5c92dafd417fdd7510e03e69a879384e8a5c7a37
Diyor-S <ruthlessfatality@gmail.com>
2025-08-03 20:26:34 +0500

    Moved item-specific data into another module

---
+++ items.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Path
+from typing import Annotated
+
+router = APIRouter(prefix="/items")
+
+
+@router.get("/")
+def list_items():
+    return [
+        "item1",
+        "item2",
+    ]
+
+@router.get("/latest/")
+def get_latest_item():
+    return {"item": {"id": "0", "name": "latest"}}
+
+# Accepting the ids that are greater than 0
+
+# The old method: with Path(..., gt=0) ... -> this means required
+# @app.get("/items/{item_id}/")
+# def get_item_by_id(item_id: int = Path(..., gt=0)):
+#     return {
+#         "item": {
+#             "id": item_id,
+#         },
+#     }
+
+# The new method: 
+@router.get("/{item_id}/")
+def get_item_by_id(item_id: Annotated[int, Path(gt=0, lt=1_000_000)]):
+    return {
+        "item": {
+            "id": item_id,
+        },
+    }
+    

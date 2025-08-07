65c4713bced332b639a8a93429df49a866d5a747
Diyor-S <ruthlessfatality@gmail.com>
2025-08-07 16:28:29 +0500

    configured db_url and Base metadata in end.py for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -19,8 +19,11 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from core.models import Base
+from core.config import settings
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", settings.db_url)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

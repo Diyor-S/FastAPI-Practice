962a290fc87f5ddb917f6489da30bbf2ba25bf0e
Diyor-S <ruthlessfatality@gmail.com>
2025-08-07 19:57:13 +0500

    created User model exported in init and ran the migration

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,11 @@
 # __all__ - is used to export those classes in one file. And also to control access to which classes are imported when using `from module import *`.
 
-__all__ = ("Base", "Product", "DBHelper", "db_helper")
+__all__ = ("Base", "Product", "DBHelper", "db_helper", "User", "Post")
 
 from .base import Base
 from .product import Product
 from .db_helper import DBHelper, db_helper
+from .user import User
+
 
 # These imports are now available in models python package. We can import those classes without knowing the exact file structure of models package.
---
+++ core/models/user.py
@@ -0,0 +1,7 @@
+from core.models import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(32), unique=True)

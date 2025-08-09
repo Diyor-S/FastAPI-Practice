4995661bb93af71ddc11aea032f32ef3d53cff4e
Diyor-S <ruthlessfatality@gmail.com>
2025-08-09 22:03:16 +0500

    created profile model and one to one relation to user

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,11 +1,21 @@
 # __all__ - is used to export those classes in one file. And also to control access to which classes are imported when using `from module import *`.
 
-__all__ = ("Base", "Product", "DBHelper", "db_helper", "User", "Post")
+__all__ = (
+    "Base",
+    "Product",
+    "DBHelper",
+    "db_helper",
+    "User",
+    "Post",
+    "Profile",
+)
 
 from .base import Base
 from .product import Product
 from .db_helper import DBHelper, db_helper
 from .user import User
 from .post import Post
+from .profile import Profile
+
 
 # These imports are now available in models python package. We can import those classes without knowing the exact file structure of models package.
---
+++ core/models/profile.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from core.models import Base
+
+from core.models.mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]

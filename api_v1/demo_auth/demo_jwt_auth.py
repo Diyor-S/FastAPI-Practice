32681bcf0d59320ba8b62a4f64910a9365d7bd71
Diyor-S <ruthlessfatality@gmail.com>
2025-08-26 19:14:22 +0500

    fixed error with the wrong access to the attributes of the Pydantic Model, before, tried to access as if it was a dict.

--- api_v1/demo_auth/demo_jwt_auth.py
+++ api_v1/demo_auth/demo_jwt_auth.py
@@ -46,10 +46,10 @@ def validate_auth_user(
         raise unauthorized_exception
     if not auth_utils.validate_password(
         password=password,
-        hashed_password=users_db[username]["password"]
+        hashed_password=users_db[username].password
     ):
         raise unauthorized_exception
-    if not users_db[username]["is_active"]:
+    if not users_db[username].is_active:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="user inactive"

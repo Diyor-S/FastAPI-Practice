97f7dd2a34cd095b4f939040a641bb198fa034cd
Diyor-S <ruthlessfatality@gmail.com>
2025-08-26 19:13:29 +0500

    included router into the main

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .products.views import router as products_router
 from .demo_auth.views import router as demo_auth_router
+from .demo_auth.demo_jwt_auth import router as demo_auth_jwt_router
 
 router = APIRouter()
+router.include_router(demo_auth_jwt_router, prefix="/jwt", tags=["JWT"])
 router.include_router(demo_auth_router, prefix="/auth", tags=["Demo Auth"])
 router.include_router(products_router, prefix="/products")

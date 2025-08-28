7b84c12e85c264ace32851966833a0b53b963ea5
Diyor-S <ruthlessfatality@gmail.com>
2025-08-28 13:27:23 +0500

    moved some logic into separate functions

--- api_v1/demo_auth/user_related_helpers.py
+++ api_v1/demo_auth/user_related_helpers.py
@@ -16,23 +16,41 @@ from api_v1.demo_auth.validation import get_current_token_payload
 from typing import Annotated
 
 
-def get_current_user(
-        payload: Annotated[dict, Depends(get_current_token_payload)]
-) -> UserSchema:
-    token_type = payload[TOKEN_TYPE_FIELD]
-    if token_type != ACCESS_TOKEN_TYPE:
+def validate_token_type(
+        payload: Annotated[dict, Depends(get_current_token_payload)],
+        token_type: str,
+):
+    current_token_type = payload[TOKEN_TYPE_FIELD]
+    if current_token_type != token_type:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token type, got {token_type}, expected {ACCESS_TOKEN_TYPE}"
+            detail=f"invalid token type, got {current_token_type}, expected {token_type}"
         )
+    return True
 
-    username = payload["username"]
+
+def get_user_by_sub(payload: Annotated[dict, Depends(get_current_token_payload)]) -> UserSchema:
+    username = payload["sub"]
     if username not in users_db:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="invalid token (user not found)"
         )
     return users_db[username]
+
+
+def get_current_user(
+        payload: Annotated[dict, Depends(get_current_token_payload)]
+) -> UserSchema:
+    validate_token_type(payload, payload[ACCESS_TOKEN_TYPE])
+    return get_user_by_sub(payload)
+
+
+def get_current_user_for_refresh(
+        payload: Annotated[dict, Depends(get_current_token_payload)]
+) -> UserSchema:
+    validate_token_type(payload, payload[REFRESH_TOKEN_TYPE])
+    return get_user_by_sub(payload)
 
 
 def get_current_active_user(user: Annotated[UserSchema, Depends(get_current_user)]) -> UserSchema:
@@ -42,22 +60,3 @@ def get_current_active_user(user: Annotated[UserSchema, Depends(get_current_user
             detail="user inactive",
         )
     return user
-
-
-def get_current_user_for_refresh(
-        payload: Annotated[dict, Depends(get_current_token_payload)]
-) -> UserSchema:
-    token_type = payload[TOKEN_TYPE_FIELD]
-    if token_type != REFRESH_TOKEN_TYPE:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token type, got {token_type}, expected {REFRESH_TOKEN_TYPE}"
-        )
-
-    username = payload["username"]
-    if username not in users_db:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid token (user not found)"
-        )
-    return users_db[username]

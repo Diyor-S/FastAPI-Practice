bbc09503fec91e903e9baefc1c4767529ecbf411
Diyor-S <ruthlessfatality@gmail.com>
2025-09-03 20:32:39 +0500

    added lifespan to fastapi 'app'

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
+from core.lifespan import lifespan
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":

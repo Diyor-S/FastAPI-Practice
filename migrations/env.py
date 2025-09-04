bd86b3258a1ee33b22d9b0abc3bafe392e2c9dd8
Diyor-S <ruthlessfatality@gmail.com>
2025-09-04 11:44:11 +0500

    configured the alembic db path, and imports

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,12 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+# My imports
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +26,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+from core.config import settings
+from core.models import Base
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", str(settings.db.url))
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

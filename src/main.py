309ee4c7ef85fdd5dc456ac6fc3beb756dbde4e9
Diyor-S <ruthlessfatality@gmail.com>
2025-09-05 12:36:09 +0500

    made default response class of the app to ORJSONResponse

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from api import router as api_router
 from core.config import settings
 from core.lifespan import lifespan
 
-app = FastAPI(lifespan=lifespan)
+
+app = FastAPI(
+    lifespan=lifespan,
+    default_response_class=ORJSONResponse,
+)
 app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":

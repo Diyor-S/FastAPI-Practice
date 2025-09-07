558c194898edc3e56113cabcaeac761ba18893fd
Diyor-S <ruthlessfatality@gmail.com>
2025-09-07 15:09:48 +0500

    registered api v1 router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
+from core.config import settings
+from .api_v1 import router as api_v1_router
 
-
-router = APIRouter()
-
+router = APIRouter(
+    prefix=settings.api.prefix,
+)
+router.include_router(
+    api_v1_router,
+)

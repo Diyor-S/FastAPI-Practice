1729e7a2fa01923d0f5b9db98e6963d35cf54fe9
Diyor-S <ruthlessfatality@gmail.com>
2025-09-07 15:25:03 +0500

    exported in models from mixins IntIdPkMixin

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,6 +1,13 @@
-__all__ = ["db_helper", "Base"]
+__all__ = [
+    "db_helper",
+    "Base",
+    "User",
+    "IntIdPkMixin",
+]
 
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+
+from .mixins import IntIdPkMixin

6849c10d6382e8edb221f42b56d115fed12abe82
Diyor-S <ruthlessfatality@gmail.com>
2025-09-07 15:25:45 +0500

    used that mixin for the User model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,6 +1,7 @@
 from core.models import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from core.models import IntIdPkMixin
 
 
-class User(Base):
+class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)

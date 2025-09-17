aa8e4cd3090dd013a6459340fe804c35f9b0d5c3
Diyor-S <ruthlessfatality@gmail.com>
2025-09-17 18:13:06 +0500

    reused the existing id mixin for the User model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,9 +1,10 @@
 from core.models import Base
+from core.models.mixins import IntIdPkMixin
 from sqlalchemy.orm import Mapped, mapped_column
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from fastapi import Depends
 
 
-class User(Base, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
     pass
 

499d734e9a670f9f4982c1afe7618f79e70a1987
Diyor-S <ruthlessfatality@gmail.com>
2025-09-19 16:07:37 +0500

    Removed unused imports from user.py

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,10 +1,7 @@
 from core.models import Base
 from core.models.mixins import IntIdPkMixin
-from sqlalchemy.orm import Mapped, mapped_column
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from fastapi import Depends
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
     pass
-

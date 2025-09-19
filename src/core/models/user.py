e90ab867ba0e07f33563994f9f202bb044397aba
Diyor-S <ruthlessfatality@gmail.com>
2025-09-19 18:11:17 +0500

    added the adapter that has the methods to get the user

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,7 +1,14 @@
 from core.models import Base
 from core.models.mixins import IntIdPkMixin
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+    @classmethod
+    def get_user_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
+

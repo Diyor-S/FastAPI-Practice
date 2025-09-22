fa21110961d800a6039ceeff539b452f02428f56
Diyor-S <ruthlessfatality@gmail.com>
2025-09-22 18:52:38 +0500

    used UserIdType

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,13 +1,15 @@
 from core.models import Base
 from core.models.mixins import IntIdPkMixin
+from core.types import UserIdType
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     @classmethod
     def get_user_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)

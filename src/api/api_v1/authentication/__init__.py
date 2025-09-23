fb0aa976760fff9095cf17f3b238bdd42072badc
Diyor-S <ruthlessfatality@gmail.com>
2025-09-23 18:08:35 +0500

    more module location corrections

--- src/api/api_v1/authentication/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = [
-    "bearer_transport",
-    "get_database_strategy",
-]
-
-from .transport import bearer_transport
-from .strategy import get_database_strategy
-from .auth_backend import auth_backend

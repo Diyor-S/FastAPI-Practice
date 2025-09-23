6c72c7c406e93fae0438b9792ad37adc3806f693
Diyor-S <ruthlessfatality@gmail.com>
2025-09-23 16:27:02 +0500

    auth_backend for login and logout.

---
+++ src/core/authentication/auth_backend.py
@@ -0,0 +1,8 @@
+from core.authentication import bearer_transport, get_database_strategy
+from fastapi_users.authentication import AuthenticationBackend
+
+auth_backend = AuthenticationBackend(
+    name="database",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)

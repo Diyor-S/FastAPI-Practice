3f15cb5f8e3bf3c0220fd3acf4921d524329c280
Diyor-S <ruthlessfatality@gmail.com>
2025-10-28 17:24:55 +0500

    formatted properly

--- src/core/authentication/user_manager.py
+++ src/core/authentication/user_manager.py
@@ -23,3 +23,25 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
         request: Optional["Request"] = None,
     ):
         log.warning(f"User %r has registered.", user.id)
+
+    async def on_after_forgot_password(
+        self,
+        user: User,
+        token: str,
+        request: Optional["Request"] = None,
+    ):
+        log.warning(
+            f"User %r has forgot their password. Reset token: %r", user.id, token
+        )
+
+    async def on_after_request_verify(
+        self,
+        user: User,
+        token: str,
+        request: Optional["Request"] = None,
+    ):
+        log.warning(
+            f"Verification requested for user %r. Verification token: %r",
+            user.id,
+            token,
+        )
